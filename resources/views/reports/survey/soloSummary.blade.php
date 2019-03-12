1f50e4f3286a697e7c1e8568885e46092a73ad1a
benson okello <benbyron24@gmail.com>
2019-03-12 09:22:12 +0300

    exists in Central Lab Spars

--- resources/views/reports/survey/soloSummary.blade.php
+++ resources/views/reports/survey/soloSummary.blade.php
@@ -59,13 +59,16 @@
                           </th>
                           @if($item[0]['step']==6 && $item[0]['upload_status'] =="")
                           <th>
-                            <span class='badge badge-success'>Complete </span>
-                            <br>
-                            <span class='badge badge-danger'>Already in Central Lab Spars</span>
+                            <!-- <span class='badge badge-success'>Complete </span> -->
+
+                            <span class='badge badge-danger'>Exists in Central Lab Spars</span>
                           </th>
 
                             @elseif($item[0]['step']==6  && $item[0]['upload_status']==0 && $item[0]['upload_status']!="")
-                          <th>  <a type="button" href="{{ url('remoteupload') }}" class="btn btn-sm btn-primary"><span>Upload</span></a>
+
+                          <th>
+                              <span class='badge badge-success'>Complete </span>
+                             <a type="button" href="{{ url('remoteupload') }}" class="btn btn-sm btn-primary"><span>Upload</span></a>
                           </th>
 
                           @elseif($item[0]['upload_status']==1)
